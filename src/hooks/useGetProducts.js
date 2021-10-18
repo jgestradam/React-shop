46db5920f2d879e541db4e98b871a03c20d12121
Joseph Estrada <jgestradam@unal.edu.co>
2021-10-18 08:23:19 -0500

    project-react-shop

---
+++ src/hooks/useGetProducts.js
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const useGetProducts = (API) => {
+  const [products, setProduct] = useState([]);
+
+	useEffect(async() => {
+		const response = await axios(API);
+		setProduct(response.data);
+	}, []);
+
+  return products;
+};
+
+export default useGetProducts;
